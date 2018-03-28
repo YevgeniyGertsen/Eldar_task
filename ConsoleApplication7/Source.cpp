29d6416420c5cd831cbceaea0bff718009579ccc
ГерценЕ <@227_TEACHER>
2018-03-28 17:31:47 +0600

    -

--- ConsoleApplication7/Source.cpp
+++ ConsoleApplication7/Source.cpp
@@ -19,7 +19,7 @@ struct Student
 };
 
 void generationStudent(Student student);
-
+void
 void main()
 {
 	Student student[10];
@@ -31,6 +31,7 @@ void main()
 	{
 		printf("%d - %c\n", i, i);
 	}*/
+	for(int)
 
 }
 
@@ -41,5 +42,20 @@ void generationStudent(Student student)
 		student.Name.fname[i] = 97 + rand() % 25;
 		student.Name.sname[i] = 192 + rand() % 25;
 		student.Name.mname[i] = 192 + rand() % 25;
+		student.usp[i] = 0 + rand() % 10;
 	}
-}
+	int min = 10;
+	for (int i = 0; i < 15; i++)
+	{
+		if (min < student.usp[i])
+			min = student.usp[i];
+	}
+	student.priznak = (min + 1) / 2;
+}		//https://github.com/YevgeniyGertsen/Eldar_task
+
+/*ƒл€ каждого студента получить одно из следующих значений 
+признака : 5 Ц отличник(только 9 и(или) 10), 
+4 Ц хорошист(нет оценок, меньше 6, но не все 9 или 10, 
+а есть 6 и(или) 7 и(или) 8), 3 Ц троечник(нет оценок, 
+меньше 4, но есть 4 и(или) 5), 2 Ц неуспевающий(есть 1 
+и(или) 2 и(или) 3).*/
